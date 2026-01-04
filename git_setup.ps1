46a86ddacb20a8e172f2bb8ec6de902c1c4716f8
VarunA-GitHub <varuna7728@gmail.com>
2026-01-04 12:40:13 +0530

    Remove temporary setup script

--- git_setup.ps1
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-try { git remote remove origin } catch {}
-git remote add origin https://github.com/VarunA-GitHub/QoS-Aware-Spatio-Temporal-Graph-Neural-Network-for-IP-Flow-Anomaly-Detection.git
-git push -u origin main
